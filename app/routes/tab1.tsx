5e93dfc9b5e36208eeb9358c3d3ab6d44a29933f
goldshoot0720 <goldshoot0720@gmail.com>
2025-05-20 22:10:17 +0800

    tab1 temp

--- app/routes/tab1.tsx
+++ app/routes/tab1.tsx
@@ -1,13 +1,14 @@
 export default function Tab1Page() {
   return (
     <div>
-      <h1>Tab1 Page</h1>
-      <p className="has-text-centered has-text-weight-light">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur
-        vel totam fugit temporibus obcaecati neque quod voluptatem minima enim
-        odio atque illum, maxime ducimus architecto eius id beatae nostrum
-        nesciunt.
-      </p>
+      <div className="container">
+        <div className="subtitle">Tab1 Page</div>
+        <div className="columns">
+          <div className="column">1111</div>
+          <div className="column">1111</div>
+          <div className="column">1111</div>
+        </div>
+      </div>
     </div>
   );
 }
